032c095f0ac85b978a5134870138ee9a284429e8
anna-shcherbakov <anna-shcherbakova@sjtu.edu.cn>
2026-03-26 12:57:44 +0800

    add tauri

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
+import process from 'node:process'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  clearScreen: false,
   plugins: [react()],
+  server: {
+    host: process.env.TAURI_DEV_HOST || false,
+    port: 5173,
+    strictPort: true,
+    hmr: process.env.TAURI_DEV_HOST
+      ? {
+          host: process.env.TAURI_DEV_HOST,
+          port: 5173,
+          protocol: 'ws',
+        }
+      : undefined,
+  },
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    target:
+      process.env.TAURI_ENV_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
+    sourcemap: Boolean(process.env.TAURI_ENV_DEBUG),
+  },
 })
